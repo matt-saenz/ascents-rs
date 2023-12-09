0fab39ddc3617482a6ee4d8f5da66e0f1ddcdf2a
matt-saenz <55054385+matt-saenz@users.noreply.github.com>
2023-12-09 12:27:57 -0600

    Take some basic user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the name of the route:");
+
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    let name = name.trim();
+
+    println!("Congrats on sending {name}!");
 }
